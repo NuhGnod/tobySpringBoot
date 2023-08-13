59432efbf6ff84b97a96bfa492bfad392a7ad7de
kimdonghun <zhfptm12@naver.com>
2023-08-13 18:19:47 +0900

    servlet 생성 등록

--- src/main/java/toby/helloboot/HellobootApplication.java
+++ src/main/java/toby/helloboot/HellobootApplication.java
@@ -1,13 +1,43 @@
 package toby.helloboot;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+//		SpringApplication.run(HellobootApplication.class, args);
+
+		ServletWebServerFactory factory = new TomcatServletWebServerFactory(); // 어떤 servletContainer를 동작시킬 수 있으니,
+		// 추상화.
+
+		WebServer webServer = factory.getWebServer( servletContext -> {
+			servletContext.addServlet("simpleServlet", new HttpServlet() {
+						@Override
+						protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+							resp.setStatus(200);
+							resp.setContentType("application/json");
+							resp.getWriter().println("Hello servlet");
+
+						}
+					})
+					.addMapping("/hello");
+		});
+		webServer.start();
+
 	}
 
 }
