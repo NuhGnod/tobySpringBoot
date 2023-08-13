cbcd8cbdc62c0160fc55c043b2a4d2f302fd05a4
kimdonghun <zhfptm12@naver.com>
2023-08-13 18:30:10 +0900

    change to frontController

--- src/main/java/toby/helloboot/HellobootApplication.java
+++ src/main/java/toby/helloboot/HellobootApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -18,26 +22,33 @@ import java.io.IOException;
 //@SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 //		SpringApplication.run(HellobootApplication.class, args);
 
-		ServletWebServerFactory factory = new TomcatServletWebServerFactory(); // 어떤 servletContainer를 동작시킬 수 있으니,
-		// 추상화.
+        ServletWebServerFactory factory = new TomcatServletWebServerFactory(); // 어떤 servletContainer를 동작시킬 수 있으니,
+        // 추상화.
 
-		WebServer webServer = factory.getWebServer( servletContext -> {
-			servletContext.addServlet("simpleServlet", new HttpServlet() {
-						@Override
-						protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-							resp.setStatus(200);
-							resp.setContentType("application/json");
-							resp.getWriter().println("Hello servlet");
+        WebServer webServer = factory.getWebServer(servletContext -> {
+            servletContext.addServlet("frontController", new HttpServlet() {
+                        @Override
+                        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                            if (req.getRequestURI().equals("/hello")
+                                    && req.getMethod().equals(HttpMethod.GET.name())) {
 
-						}
-					})
-					.addMapping("/hello");
-		});
-		webServer.start();
+                                String name = req.getParameter("name");
+                                resp.setStatus(HttpStatus.OK.value());
+                                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                                resp.getWriter().println("simpleServlet name : " + name);
+                            } else {
+                                resp.setStatus(HttpStatus.NOT_FOUND.value());
 
-	}
+                            }
+                        }
+                    })
+                    .addMapping("/*");
+        });
+        webServer.start();
+
+    }
 
 }
