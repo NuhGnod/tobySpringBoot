107d29b7e7522e6b3f1085e45bd21e77c09a2e89
kimdonghun <zhfptm12@naver.com>
2023-08-14 23:16:57 +0900

    section6

--- src/main/java/toby/helloboot/config/MyAutoConfigImportSelector.java
+++ src/main/java/toby/helloboot/config/MyAutoConfigImportSelector.java
@@ -1,16 +1,22 @@
 package toby.helloboot.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
-public class MyAutoConfigImportSelector implements DeferredImportSelector {
+import java.util.stream.StreamSupport;
 
+public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "toby.helloboot.config.autoconfig.DispatcherServletConfig",
-                "toby.helloboot.config.autoconfig.TomcatWebServerConfig"
-        };
+        ImportCandidates load = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        String[] strings = StreamSupport.stream(load.spliterator(), false).toArray(String[]::new);
+        return strings;
     }
 }
--- src/main/java/toby/helloboot/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/toby/helloboot/config/autoconfig/DispatcherServletConfig.java
@@ -3,7 +3,9 @@ package toby.helloboot.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import toby.helloboot.config.MyAutoConfiguration;
 
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     @Bean
